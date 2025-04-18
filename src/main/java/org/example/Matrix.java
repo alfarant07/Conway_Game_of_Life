ce12a9a5ba1e46a3c3e5a93268b61779c6726cab
Anthony Alfaro <alfaro2005@yahoo.com>
2025-04-18 15:44:21 -0500

    Implemented documentation

--- src/main/java/org/example/Matrix.java
+++ src/main/java/org/example/Matrix.java
@@ -10,19 +10,19 @@ public class Matrix {
     }
         public Matrix(int rows, int columns) {
         if (rows < 1 || columns < 1) {throw new IllegalArgumentException("Invalid matrix size");}
-        this.data = new int[rows][columns];
+        this.data = new int[rows][columns]; //initializing data to given construct params
     }
 
     public Matrix(int[][] array){
         if(array ==null ) {
-            throw new IllegalArgumentException("Invalid matrix");
-        } else if (array.length<1 | array[0].length<1) {
-            throw new IllegalArgumentException("Invalid matrix size");
+            throw new IllegalArgumentException("Invalid matrix");//catching invalid input
+        } else if (array.length<1 | array[0].length<1) { //catching invalid size
+            throw new IllegalArgumentException("Invalid matrix size");//throwing exception
         }
-        this.data= array;
+        this.data= array;//if inputs are good assign data->array
     }
     public int rowLength(){
-        return data.length;
+        return data.length; //returning the length of our row data field
     }
     public int colLength(){
         return data[0].length;
--- src/main/java/org/example/MatrixOutlinePrinter.java
+++ src/main/java/org/example/MatrixOutlinePrinter.java
@@ -5,21 +5,21 @@ import java.util.Arrays;
 public class MatrixOutlinePrinter implements MatrixPrinter {
 
     public String printMatrix(Matrix matrix) {
-        char[][] matrixOutline = new char[matrix.rowLength()+2][matrix.colLength()+2];
-        int matrixOutlineRowLength =matrixOutline.length;
+        char[][] matrixOutline = new char[matrix.rowLength()+2][matrix.colLength()+2]; //accounting for "+" cells in the board
+        int matrixOutlineRowLength =matrixOutline.length; //defining our boundaries for loop
         int matrixOutlineColLength = matrixOutline[0].length;
 
         for (int rowIter = 0; rowIter<matrixOutlineRowLength; rowIter++){
             for(int colIter = 0; colIter < matrixOutlineColLength;colIter++){
                 if((rowIter ==0 || rowIter==matrixOutlineRowLength-1) && (colIter == matrixOutlineColLength-1 || colIter == 0)){
-                    matrixOutline[rowIter][colIter] = '+';
+                    matrixOutline[rowIter][colIter] = '+'; //conditional for corner cells
                 } else if (rowIter==0|| rowIter == matrixOutlineRowLength-1) {
-                    matrixOutline[rowIter][colIter] = '|';
+                    matrixOutline[rowIter][colIter] = '|';//conditional for vertical cells
                 } else if (colIter==0 ||colIter== matrixOutlineColLength-1) {
-                    matrixOutline[rowIter][colIter]='-';
+                    matrixOutline[rowIter][colIter]='-'; //conditional for horizontal cells
                 }
             }
         }
-        return Arrays.deepToString(matrixOutline);
+        return Arrays.deepToString(matrixOutline); //matrix conversion into a string
     }
 }
