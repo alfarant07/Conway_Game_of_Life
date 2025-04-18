1f5eee4ffe4cd04500b15d476f2b56501728518f
Anthony Alfaro <alfaro2005@yahoo.com>
2025-04-17 20:21:37 -0500

    Add Matrix constructors
    Created two constructors for matrix class. One takes rows and colums and
    initializes data to a matrix of size[row][col]the other takes a two
    dimmensional matrix as a parameter and assigns data to the given matrix.

--- src/main/java/Matrix.java
+++ src/main/java/Matrix.java
@@ -7,19 +7,19 @@ public class Matrix {
     public void setData(int[][] matrix){
         this.data = matrix;
     }
-//        public Matrix(int rows, int columns) {
-//        if (rows < 1 || columns < 1) {throw new IllegalArgumentException("Invalid matrix size");}
-//        this.data = new int[rows][columns];
-//    }
-//
-//    public Matrix(int[][] array){
-//        if(array ==null ) {
-//            throw new IllegalArgumentException("Invalid matrix");
-//        } else if (rowLength(array)<1 || colLength(array)<1) {
-//            throw new IllegalArgumentException("Invalid matrix size");
-//        }
-//        this.data= array;
-//    }
+        public Matrix(int rows, int columns) {
+        if (rows < 1 || columns < 1) {throw new IllegalArgumentException("Invalid matrix size");}
+        this.data = new int[rows][columns];
+    }
+
+    public Matrix(int[][] array){
+        if(array ==null ) {
+            throw new IllegalArgumentException("Invalid matrix");
+        } else if (rowLength(array)<1 || colLength(array)<1) {
+            throw new IllegalArgumentException("Invalid matrix size");
+        }
+        this.data= array;
+    }
     public int rowLength(int[][] matrix){
         return matrix.length;
     }
