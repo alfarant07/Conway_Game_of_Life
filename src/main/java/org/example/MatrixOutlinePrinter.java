10b58d174434d4c5a683cd4ff45e98cd8974ca57
Anthony Alfaro <alfaro2005@yahoo.com>
2025-04-18 15:04:06 -0500

    Add MatrixOutlinePrinter class
    Added MatrixOutlinePrinter with printMatrix method. Takes a Matrix
    input and builds a 2dim char array with +2row and col size to account
    for corner cells.

---
+++ src/main/java/org/example/MatrixOutlinePrinter.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class MatrixOutlinePrinter implements MatrixPrinter {
+
+    public String printMatrix(Matrix matrix) {
+        char[][] matrixOutline = new char[matrix.rowLength()+2][matrix.colLength()+2];
+        int matrixOutlineRowLength =matrixOutline.length;
+        int matrixOutlineColLength = matrixOutline[0].length;
+
+        for (int rowIter = 0; rowIter<matrixOutlineRowLength; rowIter++){
+            for(int colIter = 0; colIter < matrixOutlineColLength;colIter++){
+                if((rowIter ==0 || rowIter==matrixOutlineRowLength-1) && (colIter == matrixOutlineColLength-1 || colIter == 0)){
+                    matrixOutline[rowIter][colIter] = '+';
+                } else if (rowIter==0|| rowIter == matrixOutlineRowLength-1) {
+                    matrixOutline[rowIter][colIter] = '|';
+                } else if (colIter==0 ||colIter== matrixOutlineColLength-1) {
+                    matrixOutline[rowIter][colIter]='-';
+                }
+            }
+        }
+        return " ";
+    }
+}
