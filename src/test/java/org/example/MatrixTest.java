496ad177fb9fb632e479f18e09e5fa560d53254c
Anthony Alfaro <alfaro2005@yahoo.com>
2025-04-17 21:09:59 -0500

    Add MatrixTest
    Implemented tests for colLength and rowLength. Utilized two test cases
    one to test if a 1 by 1 matrix can be made and returns the appropriate
    value followed by a matrix of arbitrary constant size and testing
    if both methods return that constant.

--- src/test/java/org/example/MatrixTest.java
+++ src/test/java/org/example/MatrixTest.java
@@ -1,4 +1,24 @@
+package org.example;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class MatrixTest {
-  
+
+    @Test
+    void rowLength() {
+        Matrix matrix = new Matrix(50000,35);
+        assertEquals(50000,matrix.rowLength());
+        Matrix matrix1 = new Matrix(1,1);
+        assertEquals(1,matrix1.rowLength());
+    }
+    @Test
+    void colLength() {
+        Matrix matrix = new Matrix(2500,500);
+        assertEquals(500,matrix.colLength());
+        Matrix matrix1 = new Matrix(1,1);
+        assertEquals(1,matrix1.colLength());
+    }
 }
