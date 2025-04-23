ba16c61d5af8f04ccea480bbe42454f60c8fee1e
Anthony Alfaro <alfaro2005@yahoo.com>
2025-04-22 23:55:39 -0500

    Fixed bug in conditional
    Modified the logic in the if as utilizing an or would
    always result in an exception.

--- src/main/java/org/example/BoolMatrixPrinter.java
+++ src/main/java/org/example/BoolMatrixPrinter.java
@@ -6,7 +6,7 @@ public class BoolMatrixPrinter extends MatrixOutlinePrinter {
         StringBuilder rowContent = new StringBuilder();//building string of the content row
         for(int colIter=0;colIter< matrix.colLength();colIter++){ //only need to iterate col as that gives us each value
             //in a given row
-            if(matrix.getData(row,colIter) != 1 || matrix.getData(row,colIter)!=0){
+            if(matrix.getData(row,colIter) != 1 && matrix.getData(row,colIter)!=0){
                 throw new IllegalArgumentException("Not a 0 or 1");
         }
             rowContent.append(matrix.getData(row,colIter));
