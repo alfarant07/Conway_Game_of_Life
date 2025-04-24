9d2de88da82cdaf2a24893fd932b7e60afa3cc29
Anthony Alfaro <alfaro2005@yahoo.com>
2025-04-23 23:43:14 -0500

    Implement addShape
    Implemented addShape method which checks if the shape fits on the board.
    If so, the method proceeds to overwrite the data in our gameboard
    with the respective shapeMatrix index data.

--- src/main/java/org/example/GameOfLife.java
+++ src/main/java/org/example/GameOfLife.java
@@ -9,9 +9,17 @@ public class GameOfLife extends Matrix {
         int shapeHeight = shapeName.rowLength();
         int shapeWidth = shapeName.colLength();
         if(shapeHeight>this.rowLength() || shapeWidth>this.colLength())throw new IllegalArgumentException("Shape is too big");
-        
+        for(int rowIter =0;rowIter<this.rowLength();rowIter++){
+            for(int colIter=0; colIter<this.colLength();colIter++){
+                this.setData(row+rowIter,colIter+colIter,shapeMatrix[rowIter][colIter]);
+                        }
+                    }
+                }
+
+            }
 
 
-    }
 
-}
+
+
+
