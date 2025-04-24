8b1c2c26747662c252557b3c5928b9f39594284a
Anthony Alfaro <alfaro2005@yahoo.com>
2025-04-23 23:29:24 -0500

    Add GameOfLife
    Implemented GameOfLife class which acts as the main board for our game.

---
+++ src/main/java/org/example/GameOfLife.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class GameOfLife extends Matrix {
+    public GameOfLife(int rows,int cols){
+        super(rows,cols);
+    }
+    public void addShape(Shape shapeName, int row, int column){
+        int[][] shapeMatrix = shapeName.getData();
+        int shapeHeight = shapeName.rowLength();
+        int shapeWidth = shapeName.colLength();
+        if(shapeHeight>this.rowLength() || shapeWidth>this.colLength())throw new IllegalArgumentException("Shape is too big");
+        
+
+
+    }
+
+}
