9e508416bec66f9a7cacb97b5e7e10745038197a
Anthony Alfaro <alfaro2005@yahoo.com>
2025-04-27 00:19:07 -0500

    Added javadocs

--- src/main/java/org/example/Matrix.java
+++ src/main/java/org/example/Matrix.java
@@ -1,24 +1,58 @@
 package org.example;
 public class Matrix {
     private int[][] data;
+
+    /**
+     *  Simple getter that retrives the entire two dim array
+     * @return data
+     */
     public int[][] getData(){
         return data;
     }
+
+    /**
+     *  A more specific getData method that allows us to get individual cells in our matrix
+     * @param row y coordinate in our array
+     * @param col x coordinate in our array
+     * @return the element in data[x][y]
+     */
     public int getData(int row, int col){
         return data[row][col];
     }
+
+    /**
+     * Assigns our data field to a two dimmensional matrix
+     * @param matrix a two dimmensional array that we set our data field to
+     */
     public void setData(int[][] matrix){
         this.data = matrix;
     }
+
+    /**
+     * A more specific setter that allows us to alter the individual cells of the data field
+     * @param row y coordinate of the grid
+     * @param col x coordinate of the grid
+     * @param value the element we wish to insert
+     */
     public void setData(int row, int col, int value){
         data[row][col] =value;
     }
 
+    /**
+     * Matrix constructor that takes the amount of rows and column and creates a matrix based on given dimmension
+     * @param rows length of the grid
+     * @param columns height of the grid
+     * we get a grid of rows*height
+     */
     public Matrix(int rows, int columns) {
         if (rows < 1 || columns < 1) {throw new IllegalArgumentException("Invalid matrix size");}
         this.data = new int[rows][columns]; //initializing data to given construct params
     }
 
+    /**
+     * Constructing a matrix object on a given twoDimmensional array
+     * @param array utilized to assign our data field to the array
+     */
     public Matrix(int[][] array){
         if(array ==null ) {
             throw new IllegalArgumentException("Invalid matrix");//catching invalid input
@@ -27,9 +61,19 @@ public class Matrix {
         }
         this.data= array;//if inputs are good assign data->array
     }
+
+    /**
+     *
+     * @return Returns the length of our Matrix
+     */
     public int rowLength(){
         return data.length; //returning the length of our row data field
     }
+
+    /**
+     *
+     * @return @return Returns the Height of our Matrix
+     */
     public int colLength(){
         return data[0].length;
     }
--- src/main/java/org/example/MatrixOutlinePrinter.java
+++ src/main/java/org/example/MatrixOutlinePrinter.java
@@ -2,8 +2,18 @@ package org.example;
 
 import java.util.Arrays;
 
-public class MatrixOutlinePrinter implements MatrixPrinter {
 
+public class MatrixOutlinePrinter implements MatrixPrinter {
+    /**
+     * printMatrix takes in a matrix and constructs a new two dimmensional matrix of matrix.rowLength()+2 and colLength+2
+     * this is necessary in order to account for the borders of the matrix. The function then iterates over the
+     * matrixRowLength and matrixColLength we can then construct the border on the three conditionals. If none of the
+     * conditionals are met we begin to fill our the content cells with the elements in our matrix. We then use
+     * String builder that iterates through every row and column and builds a string representation of the row adds a
+     * new line character and finally returns the string representation*
+     * @param matrix gives a matrix objects and creates a string representation
+     * @return the string version of our matrix
+     */
     public String printMatrix(Matrix matrix) {
         char[][] matrixOutline = new char[matrix.rowLength()+2][matrix.colLength()+2]; //accounting for "+" cells in the board
         int matrixOutlineRowLength =matrixOutline.length; //defining our boundaries for loop
@@ -29,12 +39,19 @@ public class MatrixOutlinePrinter implements MatrixPrinter {
             for(int j=0;j<matrixOutline[0].length; j++){
                 formattedMatrix.append(matrixOutline[i][j]);
             }
-            if(i==matrixOutline.length-1)break;
+            if(i==matrixOutline.length-1)break; //avoids redundant \n on last iteration
              formattedMatrix.append("\n");
         }
         return formattedMatrix.toString();
     }
-    protected String rowPrinter(Matrix matrix, int row){
+
+    /**
+     *  initializes every cell of our char array to an empty char
+     * @param matrix takes a matrix and assigns the cell to a space
+     * @param row needed for overwritten method which alters the data at row and column
+     * @return the string representation of a row
+     */
+    protected String rowPrinter(Matrix matrix,int row){
         StringBuilder rowContent = new StringBuilder();
         for(int colIter=0;colIter< matrix.colLength();colIter++){
             rowContent.append(' ');
