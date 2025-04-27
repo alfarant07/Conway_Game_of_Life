00b66a4b41a4a1a96d3c591a79d17bc8ede02ed3
Anthony Alfaro <alfaro2005@yahoo.com>
2025-04-26 23:06:39 -0500

    Fixed bug global variables
    When implementing test for ten steps array null pointer would be thrown
    as the intended global variables were being assigned locally.

--- src/test/java/org/example/GameOfLifeTest.java
+++ src/test/java/org/example/GameOfLifeTest.java
@@ -12,9 +12,11 @@ class GameOfLifeTest {
     private Shape glider;
     private Shape blinker;
     private Shape blinkerOscilated;
+    private Shape gliderAtTwoSteps;
+    private int[][] gliderTwoSteps;
     private GameOfLife gameBoard;
     private GameOfLife gameBoardExpected;
-    private int[][] glider4steps;
+    private int[][] glider2steps;
 
     @BeforeEach
     public void globalVariables(){
@@ -27,21 +29,64 @@ class GameOfLifeTest {
         int[][] blinkerMatrixOscilated = {{0,0,0 },
                 {1,1,1},
                 {0,0,0}};
-        int[][] glider4steps ={ {0,0,1},
+        int[][] gliderTwoSteps ={ {0,0,1},
                 {1,0,1},
                 {0,1,1}
         };
-        Shape glider = new Shape(name.Glider,gliderMatrix);
-        Shape blinkerOscilated = new Shape(name.Blinker,blinkerMatrixOscilated);
+         glider = new Shape(name.Glider,gliderMatrix);
+         blinkerOscilated = new Shape(name.Blinker,blinkerMatrixOscilated);
+         blinker = new Shape(name.Blinker,blinkerMatrix);
+         gliderAtTwoSteps = new Shape(name.Glider,gliderTwoSteps);
+
+    }
+    @Test
+    void mainFourSteps() {
+        int i=0;
         GameOfLife gameBoardExpected = new GameOfLife(10,10);
         GameOfLife gameBoard = new GameOfLife(10,10);
-        Shape blinker = new Shape(name.Blinker,blinkerMatrix);
+        gameBoard.addShape(blinker,5,5);
+        gameBoard.addShape(glider,0,0);
+        gameBoardExpected.addShape(glider,1,1);
+        gameBoardExpected.addShape(blinker,5,5);
+        BoolMatrixPrinter printedGameBoard = new BoolMatrixPrinter();
+        String boardVisualizer=printedGameBoard.printMatrix(gameBoard);
+        String boardExpected=printedGameBoard.printMatrix(gameBoardExpected);
+        while (i<4){
+            gameBoard.step();
+            boardVisualizer=printedGameBoard.printMatrix(gameBoard);
+            //System.out.println(boardVisualizer);
+            i++;
+        }
+        //System.out.println(boardExpected);
+        //System.out.println(boardVisualizer);
+        assertArrayEquals(gameBoard.getData(), gameBoardExpected.getData());
 
     }
 
 
     @Test
     void mainTenStep(){
+        int i=0;
+        GameOfLife gameBoardExpected = new GameOfLife(10,10);
+        GameOfLife gameBoard = new GameOfLife(10,10);
+        gameBoard.addShape(blinker,7,5);
+        gameBoard.addShape(glider,0,0);
+        gameBoardExpected.addShape(gliderAtTwoSteps,3,2);
+        gameBoardExpected.addShape(blinker,7,5);
+        BoolMatrixPrinter printedGameBoard = new BoolMatrixPrinter();
+        String boardVisualizer=printedGameBoard.printMatrix(gameBoard);
+        String boardExpected=printedGameBoard.printMatrix(gameBoardExpected);
+        System.out.println(boardVisualizer);
+
+        while (i<10){
+            gameBoard.step();
+            boardVisualizer=printedGameBoard.printMatrix(gameBoard);
+            //System.out.println(boardVisualizer);
+            i++;
+        }
+        System.out.println(boardExpected);
+        System.out.println(boardVisualizer);
+        assertArrayEquals(gameBoard.getData(), gameBoardExpected.getData());
 
     }
 }
